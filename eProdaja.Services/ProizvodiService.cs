cf9a76a02f2bcee73f81dbf36aa70ace736d1ab5
NzAmr <82897026+NzAmr@users.noreply.github.com>
2023-04-22 20:48:52 +0200

    V1

---
+++ eProdaja.Services/ProizvodiService.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace eProdaja.Services
+{
+    public class ProizvodiService : IProizvodiService
+    {
+        public List<Proizvodi> ProizvodiList =
+            new List<Proizvodi>() { new Proizvodi() { ID = 1, Naziv = "Laptop" }, new Proizvodi() { ID = 2, Naziv = "Mis" } };
+        public IEnumerable<Proizvodi> Get()
+        {
+            ProizvodiList.Add(new Proizvodi() { Naziv = "Test DI",ID = -1});
+            return ProizvodiList;
+        }
+
+        public Proizvodi GetById(int id)
+        {
+            return ProizvodiList.FirstOrDefault(x => x.ID == id);
+        }
+
+        
+    }
+}
